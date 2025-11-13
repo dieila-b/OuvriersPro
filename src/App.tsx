d7085b23a5c3eb82657b4f062cc4b927a147391f
dieila-b <dieilabarry@outlook.com>
2025-11-14 00:34:16 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { LanguageProvider } from "@/contexts/LanguageContext";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import InscriptionOuvrier from "./pages/InscriptionOuvrier";
+import WorkerDetail from "./pages/WorkerDetail"; // 👈 nouvelle page fiche ouvrier
 
 const queryClient = new QueryClient();
 
@@ -19,11 +20,16 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
+            {/* Page d'accueil */}
             <Route path="/" element={<Index />} />
 
             {/* Page d'inscription des ouvriers */}
             <Route path="/inscription-ouvrier" element={<InscriptionOuvrier />} />
 
+            {/* Fiche détaillée ouvrier */}
+            <Route path="/ouvrier/:id" element={<WorkerDetail />} />
+
+            {/* Route de secours */}
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
