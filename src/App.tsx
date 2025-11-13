8a8d51541588f54d5f7b70530670edc10065609e
dieila-b <dieilabarry@outlook.com>
2025-11-13 14:41:25 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import InscriptionOuvrier from "./pages/InscriptionOuvrier";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,10 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+
+            {/* Page d'inscription des ouvriers */}
+            <Route path="/inscription-ouvrier" element={<InscriptionOuvrier />} />
+
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
