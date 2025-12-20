84379e93229dcb001308af03868cc4574bc60a6b
dieila-b <dieilabarry@outlook.com>
2025-12-21 00:55:27 +0100

    Update Search.tsx

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,14 +1,16 @@
 // src/pages/Search.tsx
 import Header from "@/components/Header";
-import SearchSection from "@/components/SearchSection";
+import WorkerSearchSection from "@/components/WorkerSearchSection";
 import Footer from "@/components/Footer";
 
 export default function SearchPage() {
   return (
-    <div className="min-h-screen flex flex-col bg-slate-50">
+    <div className="min-h-screen flex flex-col bg-white overflow-x-hidden">
       <Header />
       <main className="flex-1 w-full">
-        <SearchSection />
+        <section className="w-full bg-white py-10 sm:py-14 lg:py-16">
+          <WorkerSearchSection />
+        </section>
       </main>
       <Footer />
     </div>
