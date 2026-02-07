2d0c755a379f84b62b67de8f21d704443bbd6425
dieila-b <dieilabarry@outlook.com>
2026-02-07 16:21:08 +0000

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,13 +7,9 @@ const config: CapacitorConfig = {
   webDir: "dist",
   bundledWebRuntime: false,
 
-  /**
-   * IMPORTANT
-   * - Ne mets PAS "server.url" si tu veux que l’émulateur charge les assets packagés (dist).
-   * - "server.url" sert UNIQUEMENT pour le live reload (dev).
-   */
   server: {
     androidScheme: "https",
+    hostname: "localhost",
     // cleartext: true, // uniquement si url http en dev
     // url: "http://192.168.x.x:5173", // uniquement en dev live reload
   },
